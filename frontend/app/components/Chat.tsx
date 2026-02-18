5814bb0d701aaca5e9e7e6ab185fc03eec32a643
Rafael Azevedo Lezama <55417566+lleafar@users.noreply.github.com>
2026-02-18 17:31:36 -0300

    refactor(Chat): remove unused SVG icon and adjust layout for better responsiveness

--- frontend/app/components/Chat.tsx
+++ frontend/app/components/Chat.tsx
@@ -36,30 +36,14 @@ export default function Chat() {
     <div className="flex flex-col justify-center items-center w-full h-full">
       {videoUrl === "" ? (
         <div className="flex flex-col justify-center items-center w-md h-full">
-          <div className="flex gap-2 text-xl">
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z"
-                />
-              </svg>
-            </span>
+          <div className="flex gap-2 text-xl">    
             <p className="text-gray-400">
               Cole o link de um vídeo do YouTube para começar a conversar!
             </p>
           </div>
         </div>
       ) : (
-        <div className="flex flex-col justify-end items-center w-xs sm:w-sm md:w-md lg:w-5xl h-full">
+        <div className="flex flex-col justify-end items-center h-full w-[80%] max-w-225">
           <div id="message-container" className="overflow-y-auto">
             <MessageList messageList={messages} />
           </div>
